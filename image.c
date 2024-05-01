362727e65799f4f17b72b3ecd0eda5af065a41b7
Omar Rojas <orojasduarte@nevada.unr.edu>
2024-05-01 01:54:57 +0000

    image.c

---
+++ image.c
@@ -0,0 +1,24 @@
+// Omar Rojas And Eden
+// CS 135
+// Team Project 9
+
+#include <stdio.h>
+
+int displayMenu(int userinput);
+
+int main(){
+	int userin;
+	
+	userin = displayMenu;
+	
+
+}
+
+int displayMenu(userinput){
+	printf("---PHOTOSHOP---\n")
+	printf("1- Load Image\n2- Display Image\n3- Edit Image")
+	sacnf("
+	
+}
+
+
