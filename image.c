53606fb900f197fb862dee73219c4c7181f7c917
Omar <orojasduarte@cs202-orojasduarte.lxd>
2024-05-03 03:13:31 +0000

    image

--- image.c
+++ image.c
@@ -3,12 +3,12 @@
 // Team Project 9
 
 #include <stdio.h>
-#define IMAGE_SIZE 512
-#define NAME_SIZE 50
+#define IMAGESIZE 512
+#define NAMESIZE 50
 
 int displayMenu(); // done
 int displayEdits(); // done
-void loadImage(int row, int col, char fileName[]);
+void loadImage(char fileName[], FILE* ptr);
 void convertPixel();
 void displayImage();
 void cropImage();
@@ -22,6 +22,11 @@ int main(){
 	int useredit;
 	// to make sure they loaded an image first befo anything
 	int isloaded = 0;
+	
+	int userImage[IMAGESIZE];
+	char imageName[50];
+	
+	FILE *userfileptr;
 	do{
 		userin = displayMenu();
 		switch(userin){
@@ -30,8 +35,10 @@ int main(){
 		break;
 		case 1:
 			// Load image call goes here
-			printf("image loader is boken T^T, just pretend i did for now \n\n");
+			loadImage(imageName,userfileptr);
+			//printf("image loader is boken T^T, just pretend i did for now \n\n");
 			isloaded = 1;
+			fclose(userfileptr);
 		break;
 		case 2:
 			if(isloaded == 1){
@@ -93,16 +100,21 @@ int displayEdits(){
 	return input;
 }
 
-void loadImage(int rows,int col, char fileName[]){
-	//I TRIED AND I GIVE UP FOR NOW I CANT GET FGETS TO WORK ALL IT DOES IT GETS '\n\0'
-	//FILE* fptr;
-	//printf("What is the name of the image file? ");
-	//fgets(Name, 50, stdin);
-	//printf("%s",Name);
-	//fptr = fopen(fileName,"w");
-	//if(fptr == NULL){
-	//	printf("Could not find an image with that filename.");
-	//}
+void loadImage(char fileName[], FILE* ptr){
+	char Name[50];
+	
+	printf("give me the name of the file with .txt to load:\n");
+	//fgets(Name,50,stdin);
+
+	scanf("%s", Name);
+
+	
+	ptr = fopen(Name,"r");
+	if(ptr == NULL){
+		printf("Cant open file");
+	}
+	
+	
 }
 
 void convertPixel(int col, int image[][col], char displayImage[][col]){
