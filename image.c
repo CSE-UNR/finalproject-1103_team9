85aad863747c0527fe8ead0d3b6781627163fb67
Eden <edenb@cs135-edenb.lxd>
2024-05-06 03:56:52 +0000

    5/5

--- image.c
+++ image.c
@@ -8,13 +8,12 @@
 
 int displayMenu(); // done
 int displayEdits(); // done
-int loadImage(int size, char fileName[], int image[][size], int* xrow, int* ycol);
-char convertPixel(int pixel);
-void displayImage(int size, int image[][size], int* xrow, int* ycol);
+int loadImage(int size, int image[][size], int* xrow, int* ycol); //done
+char convertPixel(int pixel); //done
+void displayImage(int size, int image[][size], int* xrow, int* ycol); //done
 void cropImage();
-void dimPic();
-void brightenPic();
-void saveImage();
+void dimBrightenPic(int choice, int xrow, int ycol, int image[][ycol]);
+int saveImage(int xrow, int ycol, int image[][ycol]);
 
 
 int main(){
@@ -24,7 +23,7 @@ int main(){
 	int xrow, ycol;
 	
 	int userImage[IMAGESIZE][IMAGESIZE];
-	char imageName[50];
+	/* char imageName[50]; */
 	
 	do{
 		userin = displayMenu();
@@ -33,7 +32,7 @@ int main(){
 			return 0;
 		break;
 		case 1:
-			isloaded = loadImage(IMAGESIZE, imageName, userImage, &xrow, &ycol);
+			isloaded = loadImage(IMAGESIZE, userImage, &xrow, &ycol);
 		break;
 		case 2:
 			if(isloaded == 1){
@@ -52,11 +51,11 @@ int main(){
 					printf("image crop is boken T^T \n\n");
 				break;
 				case 2:
-					//Dim call goes here
-					printf("image dim is boken T^T \n\n");
+					dimBrightenPic(useredit, xrow, ycol, userImage);
+					//printf("image dim is boken T^T \n\n");
 				break;
 				case 3:
-					//Brighten call goes here
+					dimBrightenPic(useredit, xrow, ycol, userImage);
 					printf("image brighten is boken T^T \n\n");
 				case 0:
 					printf("\n\n");
@@ -95,7 +94,7 @@ int displayEdits(){
 	return input;
 }
 
-int loadImage(int size, char fileName[], int image[][size], int* xrow, int* ycol){
+int loadImage(int size, int image[][size], int* xrow, int* ycol){
 	char Name[50];
 	int rowInd = 0, colInd = 0;
 	FILE* ptr;
@@ -108,72 +107,52 @@ int loadImage(int size, char fileName[], int image[][size], int* xrow, int* ycol
 	if(ptr == NULL){
 		printf("Cant open file");
 		return 0;
-	
 	}
-// Gets the size of array (col for some reason is off by one)
-	char getsize[size][size];
+	
  	char temp[size][size];
-	colInd=0;
-	for(rowInd = 0; rowInd < size && fscanf(ptr, "%c", &getsize[rowInd][colInd]) == 1; rowInd++){
-		printf("\nrow ");
-		colInd = 0;
-		while(fscanf(ptr, "%c", &getsize[rowInd][colInd]) == 1 && getsize[rowInd][colInd] != '\n' && colInd < size){
-			printf("col ");
+ 	char getColSize;
+	
+	for(rowInd = 0; rowInd < size && fscanf(ptr, "%c", &temp[rowInd][colInd]) == 1; rowInd++){
+		colInd = 1;
+		while(fscanf(ptr, "%c", &temp[rowInd][colInd]) == 1 && temp[rowInd][colInd] != '\n' && colInd < size){
 			colInd++;
 		}
+		colInd = 0;
 	}
-// resets the scans
-	fclose(ptr);
-	ptr = fopen(Name, "r");
-// asigns the size of array
-	int numCol = colInd + 1;
-	int numRow = rowInd;
-	*xrow = numRow;
-	*ycol = numCol;
-	printf("\ncolums: %d \nrows: %d \n", numCol, numRow);
-// gets the array in char using the sizes 
-	for(int i = 0; i <= numRow; i++){
-		for(int j = 0; j <= numCol; j++){
-			fscanf(ptr,"%c", &temp[i][j]);
-			if(temp[i][j] == '\n'){
-				fscanf(ptr, "\n");
-			}
-		}
+	while(temp[0][colInd] < size && temp[0][colInd] != '\n'){
+		colInd++;
 	}
 
 // test print of char
-	for(int i = 0; i <= numRow; i++){
-		for(int j = 0; j <= numCol; j++){
+	for(int i = 0; i <= rowInd; i++){
+		for(int j = 0; j <= colInd; j++){
 			printf("%c", temp[i][j]);
 		}
-	}
-// converts char array into ints
-	for (int i = 0; i < numRow; i++) {
-	        for (int j = 0; j < numCol; j++) {
+	} 
+	
+	for (int i = 0; i < rowInd; i++) {
+	        for (int j = 0; j < colInd; j++) {
 	            image[i][j] = temp[i][j] - '0';
 	        }
-	    }
+	}
 	    
 // test print of int array
-	for(int i = 0; i < numRow; i++){
+	for(int i = 0; i < rowInd; i++){
 		printf("\n");
-		for(int j = 0; j < numCol; j++){
+		for(int j = 0; j < colInd; j++){
 			printf("%d", image[i][j]);
 		}
 	}
  	
 	
-	printf("\n\nImage successfullay loaded\n\n");
-	fclose(ptr);
+	printf("\n\nImage successfully loaded\n\n");
 	int check = 1;
 	
 	return check;
-	
-	
 }
 void displayImage(int size, int image[][size], int* xrow, int* ycol){
 	// to see the size passes
-	printf("\ncolums: %d \nrows: %d \n", *ycol, *xrow);
+	printf("\ncolumns: %d \nrows: %d \n", *ycol, *xrow);
 	//to print
 	for(int i = 0; i < *xrow; i++){
 		printf("\n");
@@ -205,6 +184,56 @@ char convertPixel(int pixel){
 		}
 }
 
+void dimBrightenPic(int choice, int xrow, int ycol, int image[][ycol]){
+	int rowInd, colInd;
+	int edit[xrow][ycol];
+	if(choice == 2){
+		for(rowInd = 0; rowInd < xrow; rowInd++){
+			for(colInd = 0; colInd < ycol; colInd++){
+				edit[rowInd][colInd] = image[rowInd][colInd] + 1;
+			}
+		}
+	}
+	if(choice == 3){
+		for(rowInd = 0; rowInd < xrow; rowInd++){
+			for(colInd = 0; colInd < ycol; colInd++){
+				edit[rowInd][colInd] = image[rowInd][colInd] - 1;
+			}
+		}
+	}
+	displayImage(ycol, edit, &xrow, &ycol);
+}
 
-
-
+int saveImage(int xrow, int ycol, int image[][ycol]){
+	int rowInd, colInd;
+	char choice;
+	char Name[50];
+	FILE* ptr;
+	
+	printf("Would you like to save your edited image to a file? [Y]es or [N]o: ");
+	scanf(" %c", &choice);
+	switch(choice){
+		case 'N':
+		case 'n':
+		return 0;
+		break;
+		case 'Y':
+		case 'y':
+		printf("Enter the file name [.txt format, no spaces]:\n");
+		scanf("%s", Name);
+		ptr = fopen(Name,"w");
+		if(ptr == NULL){
+			printf("Cant open file");
+			return 0;
+		}
+		for(rowInd = 0; rowInd < xrow; rowInd++){
+			for(colInd = 0; colInd < ycol; colInd++){
+				fprintf(ptr, "%d", image[rowInd][colInd]);
+			}
+			fprintf(ptr, "\n");
+		}
+		default:
+		printf("Invalid choice\n");
+		}
+	return 0;	
+}
